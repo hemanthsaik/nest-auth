c3621232e5eb0f9f97c7edca4f8a8fd818311b7a
HemanthSai K <khemanthsai01@gmail.com>
2023-12-18 21:43:25 +0530

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+interface UserRequest extends Request {
+  user: any;
+}
+
+@Injectable()
+export class AppService {
+  googleLogin(req: UserRequest, res: Response) {
+    if (!req.user) {
+      return res.redirect('/500');
+    }
+    res.cookie('auth-payload', req.user, { httpOnly: true });
+
+    return res.redirect('/');
+  }
+}
